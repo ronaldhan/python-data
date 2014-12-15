e5ca52ce546e8cbbd6a1408f846ae46437ab9ae4
ronaldhan <ronald.han7@gmail.com>
2014-12-15 10:22:59 +0800

    百度POI获取，适应返回结果total为0的情况

--- ftbdditu.py
+++ ftbdditu.py
@@ -18,27 +18,30 @@ for k, v in catalogs.iteritems():
             if result['status'] == 0:
                 #正常返回结果,获取结果总数
                 total = result['total']
-                times = total/int(params['page_size'])
-                times += 1
-                #分页取回结果
-                for i in range(times):
-                    params['page_num'] = str(i)
-                    rr = requests.get(BD_API_PLACE, params=params)
-                    fresult = rr.json()
-                    if fresult['status'] == 0:
-                        results = fresult['results']
-                        for item in results:
-                            mysqlconn.insert(mysql_tablename,
-                                             name=item['name'],
-                                             address=item['address'],
-                                             lng=str(item['location']['lng']),
-                                             lat=str(item['location']['lat']),
-                                             catalog=catalog,
-                                             subcatalog=word)
-                        mysqlconn.commit()
-                    else:
-                        print 'XXXXX %s %s bound:%s page:%s  not finished XXXXX' % (catalog, word, str(key), str(i))
-                    print '~~~~~ %s %s bound:%s page:%s finished ~~~~~' % (catalog, word, str(key), str(i))
+                if total:
+                    times = total/int(params['page_size'])
+                    times += 1
+                    #分页取回结果
+                    for i in range(times):
+                        params['page_num'] = str(i)
+                        rr = requests.get(BD_API_PLACE, params=params)
+                        fresult = rr.json()
+                        if fresult['status'] == 0:
+                            results = fresult['results']
+                            for item in results:
+                                mysqlconn.insert(mysql_tablename,
+                                                 name=item['name'],
+                                                 address=item['address'],
+                                                 lng=str(item['location']['lng']),
+                                                 lat=str(item['location']['lat']),
+                                                 catalog=catalog,
+                                                 subcatalog=word)
+                            mysqlconn.commit()
+                        else:
+                            print 'XXXXX %s %s bound:%s page:%s  not finished XXXXX' % (catalog, word, str(key), str(i))
+                        print '~~~~~ %s %s bound:%s page:%s finished ~~~~~' % (catalog, word, str(key), str(i))
+                else:
+                    pass
             else:
                 print 'XXXXX %s %s bound:%s not finished XXXXX' % (catalog, word, str(key))
             print '~~~~~ %s %s bound:%s finished ~~~~~' % (catalog, word, str(key))
