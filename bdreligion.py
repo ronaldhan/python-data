5642152b18eb9b037bfec5c5c1c8a41a81615a65
ronaldhan <ronald.han7@gmail.com>
2015-01-26 10:14:49 +0800

    对区域分区的代码进行重构，新输入模式为m*n型，便于计算任意区域划分

--- bdreligion.py
+++ bdreligion.py
@@ -11,10 +11,15 @@ religions = {}
 coors = raw_input("输入坐标:\n")
 coors = '39.768,116.06;39.904,116.474'
 print '输入坐标为：%s' % coors
-parts = int(raw_input("输入分区数（偶数）:\n"))
-print '输入分区数为%d' % parts
-if parts < 4:
-    parts = 4
+print '分区模式，格式: %s(横行)*%s(纵行)'
+parts = raw_input("输入分区模式:\n")
+# todo 对输入的模式通过正则表达式进行判断，满足'整数*整数'的模式
+if '*' in parts:
+    part = parts.split('*')
+    XP = part[0]
+    YP = part[1]
+else:
+    print '输入模式有误'
 clist = coors.split(';')
 LW = float(clist[0].split(',')[0])
 LJ = float(clist[0].split(',')[1])
@@ -22,22 +27,20 @@ TW = float(clist[1].split(',')[0])
 TJ = float(clist[1].split(',')[1])
 DJ = TJ - LJ
 DW = TW - LW
-for i in range(parts):
-    ct = []
-    if i < parts/2:
-        p1 = LW
-        p2 = LJ + 2*DJ*i/parts
-        p3 = LW + DW/2
-        p4 = LJ + 2*DJ*(i + 1)/parts
-    else:
-        p1 = LW + DW/2
-        p2 = LJ + 2*DJ*(i - parts/2)/parts
-        p3 = LW + DW
-        p4 = LJ + 2*DJ*(i - parts/2 + 1)/parts
-    ct.append(str(p1))
-    ct.append(str(p2))
-    ct.append(str(p3))
-    ct.append(str(p4))
-    religions[i + 1] = ct
+count = 0
+
+for i in range(YP):
+    for j in range(XP):
+        ct = []
+        p1 = LW + i*DW/YP
+        p2 = LJ + j*DJ/XP
+        p3 = LW + (i + 1)*DW/YP
+        p4 = LJ + (j + 1)*DJ/XP
+        ct.append(str(p1))
+        ct.append(str(p2))
+        ct.append(str(p3))
+        ct.append(str(p4))
+        count += 1
+        religions[count] = ct
 for k, v in religions.iteritems():
     print '%d:%s' % (k, v)
